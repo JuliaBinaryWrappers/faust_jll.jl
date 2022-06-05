d360d600928d47749d5f8a8852aa394877ac061c
jlbuild <juliabuildbot@gmail.com>
2022-06-05 15:11:59 +0000

    faust_jll build 2.40.10+0

--- src/faust_jll.jl
+++ src/faust_jll.jl
@@ -2,6 +2,8 @@
 baremodule faust_jll
 using Base
 using Base: UUID
+using LazyArtifacts
+Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("faust")
